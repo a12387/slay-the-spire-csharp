7777a0225ee1a6416f1f865d834c7007a476886e
a12387 <1364219910@qq.com>
2024-12-20 02:39:10 +0800

    Add card demo

---
+++ SlayTheSpire/Game/Cards/Red/Strike.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlayTheSpire.Game.Cards.Red
+{
+    internal class Strike : AbstractCard
+    {
+        public Strike() : base("Strike", 1, CardColor.Red, CardRarity.Basic, CardTarget.Enemy, CardType.Attack)
+        {
+            IsRetain = false;
+            IsInnate = false;
+            IsExhaust = false;
+            IsEthereal = false;
+            BaseDamage = 6;
+        }
+        public override void OnUse(AbstractPlayer user, AbstractCreature target)
+        {
+            user.Attack(BaseDamage, target);
+        }
+    }
+}
